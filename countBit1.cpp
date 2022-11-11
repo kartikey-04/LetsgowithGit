2b6df6d7a013b370c0c5fdc312dccab5fdbce238
Kartikey Shrivastav <kartikeyshriofficial2003@gmail.com>
2022-11-11 23:56:26 +0530

    count bit which is 1

---
+++ countBit1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cin>>num;
+    int count=0;
+    while(num!=0){
+        if(num&1)
+        count++;
+
+        num=num>>1;
+
+    }
+    cout<<count<<endl;
+}
